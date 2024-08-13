f051c7e00bae09a50cc60c57dc6af12d44941993
Hector Ernesto Valdes Ortega <hectorvaldes0808@gmail.com>
2024-08-12 22:21:56 -0600

    Agregar archivos de proyecto.

---
+++ WebAPIAutores/Controllers/AuthorsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebAPIAutores.Entities;
+
+namespace WebAPIAutores.Controllers
+{
+    [ApiController]
+    [Route("/api/authors")]
+    public class AuthorsController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public AuthorsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Author>>> Get()
+        {
+            return await context.Authors.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Author>> GetAuthor(int id)
+        {
+            return await context.Authors.FindAsync(id);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Author author)
+        {
+            context.Add(author);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(Author author, int id)
+        {
+            if (author.Id != id)
+            {
+                return BadRequest("El id del autor no coincide con el ID de la URL");
+            }
+
+            context.Update(author);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var author = await context.Authors.FindAsync(id);
+            if (author == null)
+            {
+                return NotFound("Id no se encuentra en DB");
+            }
+
+            context.Remove(author);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+    }
+}
---
+++ WebAPIAutores/Startup.cs
@@ -0,0 +1,64 @@
+﻿
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace WebAPIAutores
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            var connectionString = Configuration.GetConnectionString("defaultConnection");
+
+            var serverVersion = ServerVersion.AutoDetect(connectionString);
+
+            services.AddControllers();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseMySql(connectionString, serverVersion)
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+
+            });
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
